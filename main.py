89f6f91d5d2a58bbe021056638c79797c78d66aa
пойда алексей петрович <aleksej.pojda@privatbank.ua>
2022-12-20 03:31:30 +0200

    почистил

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request
 import yaml, subprocess
 from os import listdir, environ
 from os.path import isfile, join
-from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
 
